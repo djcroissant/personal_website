d388141af7c4d040713122e9a8f4804a8c584092
Derek Covey <derek.covey@gmail.com>
2018-05-24 15:14:04 -0700

    Troubleshoot deploy issue

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,6 +5,10 @@ from .base import env
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+import django
+django.setup()
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['derekcovey.me'])
 
