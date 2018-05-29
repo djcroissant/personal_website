ff39181bd2b75f99523a781ae21f0a5314fbff35
Derek Covey <derek.covey@gmail.com>
2018-05-29 10:38:19 -0700

    Update email settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -112,12 +112,12 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     'DJANGO_DEFAULT_FROM_EMAIL',
-    default='Personal Website <noreply@derekcovey.me>'
+    default='Derek Covey <noreply@derekcovey.me>'
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Personal Website]')
+EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[derekcovey.me]')
 
 # ADMIN
 # ------------------------------------------------------------------------------
