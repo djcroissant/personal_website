9018e8c2d38c1c3d381a7994677c7af5a15de5e3
Derek Covey <derek.covey@gmail.com>
2018-05-29 09:19:46 -0700

    Add allowed hosts to settings file

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,7 @@ import django
 django.setup()
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['derekcovey.me'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.derekcovey.me', '.derekcovey.herokuapp.com'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
