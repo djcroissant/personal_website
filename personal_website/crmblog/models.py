44d291e98ad02c508b39bebf3facb6d44f6cba48
Derek Covey <derek.covey@gmail.com>
2018-06-01 09:45:48 -0700

    Add model for blog post

--- personal_website/crmblog/models.py
+++ personal_website/crmblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from general.models import TimeStampedModel
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=255)
+    tagline = models.CharField(max_length=255)
+    photo = models.FileField(upload_to='static/images/crmblog/post_photos')
+    content = TextField
