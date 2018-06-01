056db99ed3b79fa2069721389d2512f09d7f33e5
Derek Covey <derek.covey@gmail.com>
2018-06-01 12:50:03 -0700

    Connect blog template to model

--- personal_website/crmblog/views.py
+++ personal_website/crmblog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 from django.contrib import messages
 
+from crmblog.models import Post
 
-class HomeView(TemplateView):
+
+class HomeView(ListView):
+    model = Post
     template_name = 'crmblog/home.html'
+
+    def get_queryset(self):
+        queryset = super(HomeView, self).get_queryset()
+        return queryset.order_by('-created')[:4]     # Only need the 4 most recent posts
