d533a1f8f3f4187aad1986555a99203b7cbf304c
Derek Covey <derek.covey@gmail.com>
2018-06-09 13:08:05 -0700

    Add link to detail view of feature article

--- personal_website/crmblog/urls.py
+++ personal_website/crmblog/urls.py
@@ -6,5 +6,5 @@ app_name = "crmblog"
 urlpatterns = [
     path("", view=views.HomeView.as_view(), name="home"),
     path("archive", view=views.PostListView.as_view(), name="archive"),
-    path("archive/<slug:slug>", view=views.PostDetailView.as_view(), name="post_detail"),
+    path("<slug:slug>", view=views.PostDetailView.as_view(), name="post_detail"),
 ]
