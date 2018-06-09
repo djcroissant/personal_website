810c680e0aad5df9e8204c894b4d4d28e3701531
Derek Covey <derek.covey@gmail.com>
2018-06-09 11:07:51 -0700

    Implement email form in crmblog

--- personal_website/crmblog/views.py
+++ personal_website/crmblog/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import FormMixin
 from django.contrib import messages
 
 from crmblog.models import Post
+from .forms import ContactForm
 
 
-class HomeView(ListView):
+class HomeView(FormMixin, ListView):
     model = Post
     template_name = 'crmblog/home.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('crmblog:home')
 
     def get_queryset(self):
         queryset = super(HomeView, self).get_queryset()
         return queryset.order_by('-created')[:4]     # Only need the 4 most recent posts
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['form'] = self.get_form()
+        return context
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'Thanks for reaching out. Your message was sent successfully.')
+        return super(HomeView, self).form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests: instantiate a form instance with the passed
+        POST variables and then check if it's valid.
+        """
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
