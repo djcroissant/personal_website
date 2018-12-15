9163cb2164ae22eccae1f87f539744a3c2e55b4b
Derek Covey <derek.covey@gmail.com>
2018-12-15 13:16:18 -0800

    Create model for bios

--- personal_website/about/models.py
+++ personal_website/about/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    "created" and "modified" fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Bio(TimeStampedModel):
+    person = models.ForeignKey(settings.AUTH_USER_MODEL,
+        null=True, on_delete=models.SET_NULL)
+    photo = models.FileField(
+        upload_to='static/images/about/bios')
+    content = MarkdownxField(blank=True)
+
+    def __str__(self):
+        return self.person.name
+
+    @property
+    def formatted_markdown(self):
+        return markdownify(self.content)
