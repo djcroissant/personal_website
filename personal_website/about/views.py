7e4d736ac882ebfc7d67759013b3913d8ac1129b
Derek Covey <derek.covey@gmail.com>
2018-12-15 17:50:32 -0800

    Send bios in order of date created

--- personal_website/about/views.py
+++ personal_website/about/views.py
@@ -7,3 +7,7 @@ from about.models import Bio
 class BioListView(ListView):
   template_name = 'about/bios.html'
   model = Bio
+
+  def get_queryset(self):
+    queryset = super(BioListView, self).get_queryset()
+    return queryset.order_by('created')
